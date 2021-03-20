f77df817d83ddc61ddd465d4a04ab859bac18c6e
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-03-19 23:10:17 -0300

    A imutabilidade no React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ export function Counter() {
     const [counter, setCounter] = useState(0);
 
     function increment() {
-        setCounter(counter + 1);
+        setCounter(counter + 1); // conceito da imutabilidade => por esse motivo o setCounter, acrescenta um novo espaço na memória para armazenar cada nova informação, neste caso cada novo click, cada nova contagem...
     }
 
     return (
